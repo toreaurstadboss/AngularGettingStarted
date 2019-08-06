4a0ab67825fc4738499e7499b66f306559c3efc3
Tore Aurstad <tore.aurstad@hemit.no>
2019-08-06 22:12:52 +0200

    Locale fix

--- APM-Start/src/app/app.module.ts
+++ APM-Start/src/app/app.module.ts
@@ -1,15 +1,15 @@
 import { BrowserModule } from '@angular/platform-browser';
-import { NgModule } from '@angular/core';
-// import { registerLocaleData } from '@angular/common';
-// import localeNor from '@angular/common/locales/nb';
-// import localeNorExtra from '@angular/common/locales/nb';
+import { NgModule, LOCALE_ID } from '@angular/core';
+import { registerLocaleData } from '@angular/common';
+import localeNor from '@angular/common/locales/nb';
+import localeNorExtra from '@angular/common/locales/nb';
 import { FormsModule } from '@angular/forms';
 import { RouterModule } from '@angular/router';
 import { AppComponent } from './app.component';
 import { ProductListComponent } from "src/app/products/product-list.component";
 import { WelcomeComponent } from "src/app/home/welcome.component";
 import { ConvertToSpacesPipe } from './shared/convert-to-spaces.pipe';
-// registerLocaleData(localeNor, 'no', localeNorExtra);
+registerLocaleData(localeNor, 'no', localeNorExtra);
 
 @NgModule({
   declarations: [
@@ -17,6 +17,9 @@ import { ConvertToSpacesPipe } from './shared/convert-to-spaces.pipe';
     ProductListComponent,
     ConvertToSpacesPipe
   ],
+  providers: [
+    { provide: LOCALE_ID, useValue: 'no'}
+  ]
   imports: [
     BrowserModule,
     FormsModule,
