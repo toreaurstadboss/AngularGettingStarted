8468df55aaf1289651ce8e6d5d69cbb241d7e0fc
Tore Aurstad <tore.aurstad@hemit.no>
2019-08-06 17:29:31 +0200

    Module 7 completed

--- APM-Start/src/app/app.module.ts
+++ APM-Start/src/app/app.module.ts
@@ -1,13 +1,14 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule, LOCALE_ID } from '@angular/core';
 import { registerLocaleData } from '@angular/common';
-import  localeNor from '@angular/common/locales/nb';
-import  localeNorExtra from '@angular/common/locales/nb';
+import localeNor from '@angular/common/locales/nb';
+import localeNorExtra from '@angular/common/locales/nb';
 import { FormsModule } from '@angular/forms';
 import { RouterModule } from '@angular/router';
 import { AppComponent } from './app.component';
 import { ProductListComponent } from "src/app/products/product-list.component";
 import { WelcomeComponent } from "src/app/home/welcome.component";
+import { ConvertToSpacesPipe } from "src/app/shared/convert-to-spaces.pipe";
 
 registerLocaleData(localeNor, 'no', localeNorExtra)
 
@@ -15,16 +16,16 @@ registerLocaleData(localeNor, 'no', localeNorExtra)
   declarations: [
     AppComponent,
     ProductListComponent,
+    ConvertToSpacesPipe
   ],
   providers: [
-    {provide: LOCALE_ID, useValue: 'no'
+    { provide: LOCALE_ID, useValue: 'no' }
   ],
   imports: [
     BrowserModule,
     FormsModule,
     RouterModule.forRoot([
       { path: '', component: AppComponent }
-
     ])
   ],
   bootstrap: [AppComponent]
