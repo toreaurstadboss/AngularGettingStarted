e511e5a1f8ec07c8fa85ba438ab6db05cb8c3336
Tore Aurstad <tore.aurstad@hemit.no>
2019-08-06 13:32:37 +0200

    Base setup of layout progressing

--- APM-Start/src/app/app.module.ts
+++ APM-Start/src/app/app.module.ts
@@ -1,14 +1,23 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
-
+import { FormsModule } from '@angular/forms';
+import { RouterModule } from '@angular/router';
 import { AppComponent } from './app.component';
+import { ProductListComponent } from "src/app/products/product-list.component";
+import { WelcomeComponent } from "src/app/home/welcome.component";
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    ProductListComponent,
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    FormsModule,
+    RouterModule.forRoot([
+      { path: '', component: AppComponent }
+
+    ])
   ],
   bootstrap: [AppComponent]
 })
