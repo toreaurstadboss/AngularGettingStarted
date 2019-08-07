0e26bf6797165230c80d04ba8123b3168006d5de
Tore Aurstad <tore.aurstad@hemit.no>
2019-08-07 16:23:44 +0200

    Adding webpack bundle analyzer

--- APM-Start/src/app/products/product-detail.guard.ts
+++ APM-Start/src/app/products/product-detail.guard.ts
@@ -1,7 +1,6 @@
 import { Injectable } from '@angular/core';
 import { CanActivate, ActivatedRouteSnapshot, RouterStateSnapshot } from '@angular/router';
 import { Observable } from 'rxjs';
-import { Promise } from "@types/q";
 import { Router } from "@angular/router";
 import { Pipe } from "@angular/core";
 
